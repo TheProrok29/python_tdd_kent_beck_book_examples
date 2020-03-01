6dec65dcc1b0b5dcdafe658e7c4d7ddd828a641e
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-01 18:27:04 +0100

    Create a first failing(not even work) test case for multiplication

---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+from financial_operations import Dollar
+
+
+class MyTestCase(unittest.TestCase):
+    def test_multiplication(self):
+        five: Dollar = Dollar(5)
+        five.times(2)
+        self.assertEqual(10, five.amount)
+
+
+if __name__ == '__main__':
+    unittest.main()
