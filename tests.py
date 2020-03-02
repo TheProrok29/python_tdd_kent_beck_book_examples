d8f88220387d9c90b96a3d761ff3f275a47434d4
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-02 15:23:41 +0100

    Refactor test_multiplication to delete unused working variable.

--- tests.py
+++ tests.py
@@ -5,10 +5,8 @@ from financial_operations import Dollar
 class MyTestCase(unittest.TestCase):
     def test_multiplication(self):
         five: Dollar = Dollar(5)
-        product: Dollar = five.times(2)
-        self.assertEqual(10, product.amount)
-        product = five.times(3)
-        self.assertEqual(15, product.amount)
+        self.assertEqual(Dollar(10), five.times(2))
+        self.assertEqual(Dollar(15), five.times(3))
 
     def test_equality(self):
         self.assertEqual(Dollar(5), Dollar(5))
