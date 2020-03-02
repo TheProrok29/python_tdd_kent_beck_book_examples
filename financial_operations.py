3069d2d237b295ae6239ccc05e2252d02f7b78d8
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-02 15:26:24 +0100

    Change amount attribute to "private", not being part of public API

--- financial_operations.py
+++ financial_operations.py
@@ -2,14 +2,13 @@ from __future__ import annotations
 
 
 class Dollar:
-    amount: int = 0
 
     def __init__(self, amount: int) -> None:
-        self.amount = amount
+        self._amount = amount
 
     def __eq__(self, other) -> bool:
         dollar: Dollar = other
-        return self.amount == dollar.amount
+        return self._amount == dollar._amount
 
     def times(self, multiplier: int) -> Dollar:
-        return Dollar(self.amount * multiplier)
+        return Dollar(self._amount * multiplier)
