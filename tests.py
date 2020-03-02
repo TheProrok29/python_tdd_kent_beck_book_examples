97ee74aec562d20c0dcd0877b09d5db8ce975196
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-02 18:05:36 +0100

    Delete redundant assertion in test equality.

--- tests.py
+++ tests.py
@@ -16,8 +16,6 @@ class MyTestCase(unittest.TestCase):
     def test_equality(self):
         self.assertEqual(Money.dollar(5), Money.dollar(5))
         self.assertNotEqual(Money.dollar(5), Money.dollar(6))
-        self.assertEqual(Money.franc(5), Money.franc(5))
-        self.assertNotEqual(Money.franc(5), Money.franc(6))
         self.assertNotEqual(Money.franc(5), Money.dollar(5))
 
     def test_currency(self):
