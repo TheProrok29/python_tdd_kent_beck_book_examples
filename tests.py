34bb3b1b2ea133f1937776d86939b43a9703ad88
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-04 16:07:49 +0100

    Change new test Expression to Money type.

--- tests.py
+++ tests.py
@@ -57,8 +57,8 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(1, Bank().rate('USD', 'USD'))
 
     def test_mixed_additions(self):
-        five_bucks: Expression = Money.dollar(5)
-        ten_francs: Expression = Money.franc(10)
+        five_bucks: Money = Money.dollar(5)
+        ten_francs: Money = Money.franc(10)
         bank: Bank = Bank()
         bank.add_rate("CHF", "USD", 2)
         result: Money = bank.reduce(five_bucks.plus(ten_francs), "USD")
