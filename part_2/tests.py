3bfc57ee4321930c60c878f917fb32b519c1c6eb
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-09 09:43:55 +0100

    Implement body of test_method.

--- part_2/tests.py
+++ part_2/tests.py
@@ -1,9 +1,9 @@
 class WasRun:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.wasRun = None
 
-    def test_method(self):
-        pass
+    def test_method(self) -> None:
+        self.wasRun = 1
 
 
 test = WasRun('test_method')
