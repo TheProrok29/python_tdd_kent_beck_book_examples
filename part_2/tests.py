5de88700f0d67239383b590d2f46434c279a04ba
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-10 11:04:35 +0100

    Add run() method to TestSuite.

--- part_2/tests.py
+++ part_2/tests.py
@@ -20,6 +20,12 @@ class TestSuite:
     def add(self, test):
         self.tests.append(test)
 
+    def run(self):
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+        return result
+
 
 class TestCase:
     def __init__(self, name: str):
