329bde98ba821e994d2546cae529cb00937797c2
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-10 10:11:52 +0100

    Add test_started() to increment TestResult counter.

--- part_2/tests.py
+++ part_2/tests.py
@@ -1,6 +1,9 @@
 class TestResult:
     def __init__(self):
-        self.runCount = 1
+        self.runCount = 0
+
+    def test_started(self):
+        self.runCount += 1
 
     def summary(self):
         return f'{self.runCount} run, 0 failed'
