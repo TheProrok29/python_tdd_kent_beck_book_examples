14f38ba507f9efb8f9361a538f727158bc5bf142
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2020-03-10 10:22:38 +0100

    Add failed result counter to TestResult.

--- part_2/tests.py
+++ part_2/tests.py
@@ -1,9 +1,13 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def test_started(self):
         self.runCount += 1
+
+    def test_failed(self):
+        self.errorCount += 1
 
     def summary(self):
         return f'{self.runCount} run, 0 failed'
